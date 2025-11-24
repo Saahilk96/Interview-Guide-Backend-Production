8f7d2de40499566f1f25dd97f99dd60858557fc2
Shiva Aleti <Shiva.Aleti@ACPL.COM>
2025-11-24 16:32:55 +0530

    updated stripe webhookk pyament status without any checking

--- main.py
+++ main.py
@@ -870,6 +870,7 @@ async def create_checkout_session(data: utils.CheckoutRequest):
             mode="payment",
             success_url="http://localhost:3000/payment-success?userId=" + data.userId,
             cancel_url="http://localhost:3000/payment-cancel",
+            customer_email=user["email"],
             metadata={
                 "user_id": data.userId  # ✓ SECURE
             }
@@ -892,30 +893,30 @@ async def stripe_webhook(request: Request):
     except Exception as e:
         return JSONResponse(status_code=400, content={"error": str(e)})
 
-    if event["type"] == "checkout.session.completed":
-        session = event["data"]["object"]
+    # if event["type"] == "checkout.session.completed":
+    session = event["data"]["object"]
 
-        user_id = session["metadata"].get("user_id")
+    user_id = session["metadata"].get("user_id")
 
-        if not user_id:
+    if not user_id:
             print("❌ user_id missing in metadata")
             return {"status": "failed"}
 
-        from bson import ObjectId
+    from bson import ObjectId
 
-        try:
-            obj_id = ObjectId(user_id)
-        except:
-            print("❌ Invalid ObjectId")
-            return {"status": "invalid-object-id"}
+    try:
+        obj_id = ObjectId(user_id)
+    except:
+        print("❌ Invalid ObjectId")
+        return {"status": "invalid-object-id"}
 
         # Update user payment status
-        await googleAuth.update_one(
-            {"_id": obj_id},
-            {"$set": {"paymentDone": True}}
-        )
+    await googleAuth.update_one(
+        {"_id": obj_id},
+        {"$set": {"paymentDone": True}}
+    )
 
-        print("✅ User payment updated:", user_id)
+    print("✅ User payment updated:", user_id)
 
     return {"status": "success"}
 
